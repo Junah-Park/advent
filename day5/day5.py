ce08614468671187ab1ad02f43804a13a819d5d4
Junah Park <park.junah95@gmail.com>
2023-12-13 10:05:08 -0800

    day5 part1.5

--- day5/day5.py
+++ day5/day5.py
@@ -4,13 +4,6 @@ def main():
     with open('input.txt') as f:
         for line in f:
             input.append(line)
-    seeds = input[0].strip().split(':')[1].strip().split(' ')
-    seedRanges = []
-    i = 0
-    while i < len(seeds):
-        seedRanges.append((seeds[i], seeds[i+1]))
-        i += 2
-    destinations = []
 
     index = 3
     seedSoil = []
@@ -20,15 +13,6 @@ def main():
         seedSoil.append((source, source + length, destination, length))
         index+=1
     
-    newSeedRanges = []
-    for seedRange in seedRanges:
-        seedStart = seedRange[0]
-        seedLength = seedRange[1]
-        for mapRange in seedSoil:
-            mapDestin = mapRange[0]
-            mapSource = mapRange[1]
-            mapLength = mapRange[2]
-
     index += 2
     soilFertilizer = []
     while input[index][0].isdigit():
@@ -77,28 +61,69 @@ def main():
         humidityLocation.append((source, source + length, destination, length))
         index+=1
 
+
+    seeds = input[0].strip().split(':')[1].strip().split(' ')
+    seedRanges = []
+    i = 0
+    while i < len(seeds):
+        seedRanges.append((seeds[i], seeds[i+1]))
+        i += 2
+
     lowestLocation = math.inf
     maps = [seedSoil, soilFertilizer, fertilizerWater, waterLight, lightTemperature,temperatureHumidity, humidityLocation]
-    index = 0
-    while index < len(seeds):
-        seedStart, seedCount = int(seeds[index]), int(seeds[index+1])
-        for mp in maps:
-            seedRanges = [[seedStart, seedCount]]
-            for ranges in mp:
-                sourceStart, sourceEnd, destinationStart, length = ranges[0], ranges[1], ranges[2], ranges[3]
-                newSeedRanges = []
-                for i in range(len(seedRanges)):
-                    seedStart = seedRanges[i][0]
-                    seedCount = seedRanges[i][1]
-                    if sourceStart <= seedStart < sourceEnd:
-                        newSeedRanges.append([seedStart, seedStart + min(seedCount, length)])
-                        break
-            if newSeed == seed:
-                seed = newSeed
-                break
-            seed = newSeed
-        lowestLocation = min(lowestLocation, seed)
-        index += 1
+    
+    destinations = []
+    for curMap in maps:
+        for mapRange in curMap:
+            mapDestin = mapRange[0]
+            mapSource = mapRange[1]
+            mapLength = mapRange[2]
+            mapStart = mapSource
+            mapEnd = mapSource + mapLength
+            newSeedRanges = []
+            for seedRange in seedRanges:
+                seedStart = seedRange[0]
+                seedLength = seedRange[1]
+                seedEnd = seedStart+seedLength
+                # cases
+                # seedStart, seedEnd, mapStart, mapEnd or mapStart, mapEnd, seedStart, seedEnd
+                if seedStart <= seedEnd <= mapSource:
+                    newSeedRanges.append([seedStart, seedEnd])
+                # seedStart, mapStart, mapEnd, seedEnd
+                if seedStart <= mapStart and mapEnd <= seedEnd:
+                    newRange1 = [seedStart, mapStart]
+                    newRange2 = [mapEnd, seedEnd]
+                    newRange3 = [mapStart, mapEnd]
+                    newSeedRanges.append(newRange1)
+                    newSeedRanges.append(newRange2)
+                    newSeedRanges.append(newRange3)
+                # mapStart, seedStart, seedEnd, mapEnd
+                if mapStart <= seedStart and seedEnd <= mapEnd:
+                    newSeedRanges.append
+                # mapStart, seedStart, mapEnd, seedEnd
+
+                seedRanges = newSeedRanges
+    
+    # index = 0
+    # while index < len(seeds):
+    #     seedStart, seedCount = int(seeds[index]), int(seeds[index+1])
+    #     for mp in maps:
+    #         seedRanges = [[seedStart, seedCount]]
+    #         for ranges in mp:
+    #             sourceStart, sourceEnd, destinationStart, length = ranges[0], ranges[1], ranges[2], ranges[3]
+    #             newSeedRanges = []
+    #             for i in range(len(seedRanges)):
+    #                 seedStart = seedRanges[i][0]
+    #                 seedCount = seedRanges[i][1]
+    #                 if sourceStart <= seedStart < sourceEnd:
+    #                     newSeedRanges.append([seedStart, seedStart + min(seedCount, length)])
+    #                     break
+    #         if newSeed == seed:
+    #             seed = newSeed
+    #             break
+    #         seed = newSeed
+    #     lowestLocation = min(lowestLocation, seed)
+    #     index += 1
     print(lowestLocation)
 
 if __name__ == "__main__":
